c1c43f8c5c18f5ba877ad56580c81d4482605605
KhawlaBENSGHIR <khawla.bensghir@etu.unice.fr>
2024-12-06 16:36:05 +0100

    g

--- BonjourMonde.java
+++ BonjourMonde.java
@@ -1,7 +1,7 @@
 // Un programme Java simple
 public class BonjourMonde {
     public static void main(String[] args) {
-        System.out.println("Bonjour, monde !");
+        System.out.println("Bonjour, monde ;; !");
     }
 }
 
